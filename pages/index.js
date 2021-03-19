f787a0a66c008f2c2690c9609233f518fea42f13
sergiodisere <sergiodisere@hotmail.com>
2021-03-19 15:37:24 +0100

    19/03

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a>Geo-SCPOMNAV!!</a>
+          Welcome to <a>Geo-SCPOMNAV!</a>
         </h1>
 
         <p className={styles.description}>
